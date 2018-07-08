721da067e4713807720835810ca8e099916abf55
Jousha <robson2004@hotmail.co.uk>
2018-07-08 16:36:24 +0100

    Remove API
    Removal of initial API listing

--- __main__.py
+++ __main__.py
@@ -1,6 +1,8 @@
 from helpers import get_inventory, get_item_name
 
-inventory = get_inventory('D7E0B1A9-8A4A-AF42-920A-0564CE2DEF025E5DCC90-1E33-416F-9847-C601130B4261')
+API_KEY = ''
+
+inventory = get_inventory(API_KEY)
 
 for line in inventory:
     if line is None:
