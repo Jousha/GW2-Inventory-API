333c02b97fc1ba82df998f7db4d6fa6385465e41
Jousha <robson2004@hotmail.co.uk>
2018-07-08 21:58:54 +0100

    Character inventories
    Add methods for pulling individual character inventories

--- helpers.py
+++ helpers.py
@@ -1,7 +1,7 @@
 import json
 import requests
 
-def get_inventory(api_token):
+def get_bank_inventory(api_token):
     '''
     Input:
         api_token - String of the api key of a valid account with inventory viewing rights
@@ -9,7 +9,21 @@ def get_inventory(api_token):
     Returns a list object containing the bank inventory associated with the api key
     '''
     inventory_api = f'https://api.guildwars2.com/v2/account/bank?access_token={api_token}'
-    return parse_data(inventory_api)
+    return _parse_data(inventory_api)
+
+def get_character_inventory(api_token, character_name):
+    '''
+    Input:
+        api_token - String of the api key of a valid account with inventory viewing rights
+        character_name - The name of the character which will be queried
+
+    Returns a list object containing the character inventory associated with the api key
+    '''
+    if (' ' in character_name):
+        character_name = '%20'.join(character_name.split(' '))
+        
+    character_api = f'https://api.guildwars2.com/v2/characters/{character_name}/inventory?access_token={api_token}'
+    return _parse_data(character_api)
 
 def get_item_name(item_id):
     '''
@@ -19,10 +33,10 @@ def get_item_name(item_id):
     Returns a string object for the item's name
     '''
     item_api = f'https://api.guildwars2.com/v2/items?ids={item_id}'
-    item_data = parse_data(item_api)
+    item_data = _parse_data(item_api)
     return item_data[0]['name']
 
-def parse_data(api_string):
+def _parse_data(api_string):
     '''
     Input:
         api_string - String of a valid api link
