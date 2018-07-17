b231354c30781eb9247074c56dc8de97825ac845
Jousha <robson2004@hotmail.co.uk>
2018-07-17 21:26:55 +0100

    Initial upload
    First upload of cli setup file

---
+++ Scripts/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='price_check',
+    version='1.0',
+    py_modules=['price_check', 'bank_item_profit'],
+    install_requires=[
+        'Click',
+        'Requests',
+    ],
+    entry_points='''
+        [console_scripts]
+        price_check=price_check:get_item_profit
+        bank_item_profit=bank_item_profit:get_bank_profit
+    ''',
+)
