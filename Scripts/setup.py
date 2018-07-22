9b2c2739aa93bf4b809dfc5a9e06352c00291edc
Jousha <robson2004@hotmail.co.uk>
2018-07-22 18:24:45 +0100

    Character profits
    Added reference to new get_character_profits file

--- Scripts/setup.py
+++ Scripts/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='price_check',
     version='1.0',
-    py_modules=['price_check', 'bank_item_profit'],
+    py_modules=['price_check', 'bank_item_profit', 'get_character_profit'],
     install_requires=[
         'Click',
         'Requests',
@@ -12,5 +12,6 @@ setup(
         [console_scripts]
         price_check=price_check:get_item_profit
         bank_item_profit=bank_item_profit:get_bank_profit
+        character_item_profit=character_item_profit:get_character_profit
     ''',
 )
